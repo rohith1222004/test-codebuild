c53123132b8eb10fa07e9779ca0c2cb5447b228d
Rohith Kumar B <92452259+rohith1222004@users.noreply.github.com>
2025-01-16 14:54:07 +0530

    Update before_install.sh

--- before_install.sh
+++ before_install.sh
@@ -1,20 +1,22 @@
-#!/bin/bash
+# #!/bin/bash
 
-# Define the S3 bucket path and the destination file path
-S3_BUCKET_PATH="s3://ssl-certs-2004/build_2025-01-08/EU_Jar/my-java-project-1.0-SNAPSHOT.jar"
-DEST_DIR="/home/ec2-user/ebs/java"
+# # Define the S3 bucket path and the destination file path
+# S3_BUCKET_PATH="s3://ssl-certs-2004/build_2025-01-08/EU_Jar/my-java-project-1.0-SNAPSHOT.jar"
+# DEST_DIR="/home/ec2-user/ebs/java"
 
-# Create the destination directory if it doesn't exist
-mkdir -p $DEST_DIR
+# # Create the destination directory if it doesn't exist
+# mkdir -p $DEST_DIR
 
-# Use the AWS CLI to copy the JAR file from S3 to the destination
-echo "Downloading JAR file from S3..."
-aws s3 cp $S3_BUCKET_PATH $DEST_DIR/app.jar
+# # Use the AWS CLI to copy the JAR file from S3 to the destination
+# echo "Downloading JAR file from S3..."
+# aws s3 cp $S3_BUCKET_PATH $DEST_DIR/app.jar
 
-# Check if the download was successful
-if [ $? -eq 0 ]; then
-  echo "JAR file downloaded successfully."
-else
-  echo "Failed to download JAR file."
-  exit 1
-fi
+# # Check if the download was successful
+# if [ $? -eq 0 ]; then
+#   echo "JAR file downloaded successfully."
+# else
+#   echo "Failed to download JAR file."
+#   exit 1
+# fi
+echo "hello world"
+sudo mkdir Hello
